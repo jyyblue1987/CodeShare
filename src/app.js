9833155add07e751f9b1a2048ee60f332b0928b7
jyyblue <jyyblue1987@outlook.com>
2021-04-16 10:35:40 +0800

    [add] show modal for code.

--- src/app.js
+++ src/app.js
@@ -8,16 +8,69 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/code_snippets/', (req, res) => {
+    var title = req.body.title;
+    var code = req.body.code;
+    var comments = [];
+    var code = new CodeSnippet({title, code, comments});
 
+    code.save()
+        .then(function(doc) {            
+            res.json({
+                code: 200,
+                data: {
+                    id: doc._id,
+                    title: doc.title,
+                    code: doc.code,
+                    comments: doc.comments
+                }
+            });
+        })
+        .catch(function(error) {
+            res.json({
+                'code': 201,
+                'message': 'Code Save Error'
+            });
+        });
 });
 
 app.post('/code_snippets/:id/comments/', (req, res) => {
-
+    CodeSnippet.findByIdAndUpdate(req.params["id"], {
+            "$push": {
+                comments: req.body["comment"]
+            }
+        }, {
+            "new": true
+        },
+        (err, docs) => {
+            if (err) {
+                res.json({
+                    "error": "The comment was not successfully added."
+                });
+            } else {
+                res.json({
+                    "message": "Change was successful",
+                    "docs": docs
+                });
+            }
+        }
+    );
 
 });
 
 app.get('/code_snippets/', (req, res) => {
-
+    CodeSnippet.find({})
+        .then(function(docs) {
+            res.json({
+                'code': 200,
+                'docs': docs
+            });
+        })
+        .catch(function(error) {
+            res.json({
+                'code': 201,
+                'message': error.message
+            });
+        });
 });
 
 const port = 3000;
--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -1,1 +1,93 @@
 // Write your client side Javascript code here
+document.addEventListener("DOMContentLoaded", function(event) { 
+    initEvents();
+    displayCodes();
+});
+
+function initEvents() {
+    var button = document.getElementById('btn-show-modal-code-snippet');
+    button.onclick = function() {
+        // show modal
+        var code_modal = document.getElementById('modal-code-snippet');
+        code_modal.style.display = 'block';
+    }
+
+    var code_submit_btn = document.getElementById('create-code-snippet');
+    code_submit_btn.onclick = async function() {
+        var title = document.getElementById('code-snippet-title').value;
+        var code = document.getElementById('code-snippet-code').value;
+
+        try {
+            var results = await fetch('/code_snippets', {
+                method: 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({title: title, code: code})
+            });
+
+            const data = await results.json();
+            addCodeData(data.data);
+
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    var close_button_array = document.getElementsByClassName('close');
+    for(var i = 0; i < close_button_array.length; i++)
+    {
+        var item = close_button_array[i];
+        item.onclick = function() {
+            var code_modal = document.getElementById('modal-code-snippet');
+            var comment_modal = document.getElementById('modal-comment');
+            code_modal.style.display = 'none';
+            comment_modal.style.display = 'none';
+        }
+    };
+    
+}
+
+async function displayCodes() {    
+    try {
+        var results = await fetch('/code_snippets');
+        const data = await results.json();
+
+        console.log(data);
+        if( data.code != 200 )
+            return;
+        
+        data.docs.forEach(item => {
+            addCodeData(item);
+        });
+        
+
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+function addCodeData(item) {
+    // find main element
+    var main = document.getElementsByTagName('main')[0];
+
+    var article = document.createElement("article");
+    var title = document.createElement("h3");
+    title.innerHTML = item['title'];
+
+    var code = document.createElement("pre");
+    code.innerHTML = item['code'];
+
+    var comments = document.createElement("ul");
+
+    comments.innerHTML = '<li>Coffee</li>' +
+                            '<li>Tea</li>' +
+                            '<li>Milk</li>';
+
+    article.appendChild(title);
+    article.appendChild(code);
+    article.appendChild(comments);
+
+    main.appendChild(article);
+}
